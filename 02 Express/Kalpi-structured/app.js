fd8e29f352b2433f4f782c51238c46b60212bb95
ShaiMesisterano <shai@mcterano.com>
2019-09-21 14:37:28 +0300

    add exercises

--- 02 Express/Kalpi-structured/app.js
+++ 02 Express/Kalpi-structured/app.js
@@ -4,15 +4,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var body_parser_1 = __importDefault(require("body-parser"));
+var cors_1 = __importDefault(require("cors"));
+var api_1 = __importDefault(require("./routers/api"));
 var app_1 = require("./configs/app");
-var log_1 = require("./services/log");
 var port = app_1.config.port;
 var app = express_1.default();
 // Custom Middleware
-app.use('*', log_1.log);
-app.get('/', function (request, response) {
-    response.end();
-});
-// app.use(bodyParser.json());
-// app.use('/api', apiRouter);
+// app.use('*', log);
+// app.get('/', (request: express.Request, response: express.Response) => {
+//     response.end(response.locals.someData);
+// });
+app.use(cors_1.default());
+app.use(body_parser_1.default.json());
+app.use('/api', api_1.default);
 app.listen(port, function () { return console.log("Server is running on port " + port); });
--- 02 Express/Kalpi-structured/app.ts
+++ 02 Express/Kalpi-structured/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import apiRouter from './routers/api';
 import { config } from './configs/app';
 import { log } from './services/log';
@@ -9,12 +10,13 @@ const port: number = config.port;
 const app: express.Application = express();
 
 // Custom Middleware
-app.use('*', log);
+// app.use('*', log);
 
 // app.get('/', (request: express.Request, response: express.Response) => {
 //     response.end(response.locals.someData);
 // });
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', apiRouter);
 
